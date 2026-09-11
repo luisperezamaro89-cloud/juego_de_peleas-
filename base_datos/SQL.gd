17ed9f58726ffb66458c303a1038b02a92bfa2a7
luisperezamaro89-cloud <luisperezamaro89@gmail.com>
2026-09-11 17:34:54 -0500

    ya logre conectar a el sql
    lo logre lo logre conectar a sql pero ay 2 asique 1 esta vacio y el otro tiene datos siiiiiiiiiiiiiiiiii

--- base_datos/SQL.gd
+++ base_datos/SQL.gd
@@ -1,45 +1,49 @@
 extends Control
 var baseDatos : SQLite
 
-# Called when the node enters the scene tree for the first time.
-func _ready() :
+func _ready():
 	baseDatos = SQLite.new()
-	baseDatos.path="res//data.db"
+	baseDatos.path = "res://data.db"
 	baseDatos.open_db()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_crear_tabla_button_down():
-	var table ={
-		"id" :{"data_type":"int","primary_key": true ,"not_null" :true ,"avo_increment": true },
-		"nombre":{"data_type" : "text"},
-		"scores": {"data_type" : "int"},
-		}
-	baseDatos.create_table("players",table)
-	
-	pass # Replace with function body.
+	var table = {
+		"id": {"data_type": "int", "primary_key": true, "not_null": true, "auto_increment": true},
+		"nombre": {"data_type": "text"},
+		"puntaje": {"data_type": "int"},
+	}
+	var resultado = baseDatos.create_table("players", table)
+	print("¿Se creó la tabla?: ", resultado)
+	print("Error (si hay): ", baseDatos.error_message)
 
-
-func _on_insertar_jugador_button_down() -> void:
-	pass # Replace with function body.
-
+func _on_insertar_jugador_button_down():
+	var data = {
+		"nombre": $Nombre.text,
+		"puntaje": int($Puntuación.text)
+	}
+	var resultado = baseDatos.insert_row("players", data)
+	print("¿Se insertó la fila?: ", resultado)
+	print("Error (si hay): ", baseDatos.error_message)
 
 func _on_ver_jugadores_button_down() -> void:
-	pass # Replace with function body.
-
+	baseDatos.query("SELECT * FROM players")
+	print("Jugadores encontrados: ", baseDatos.query_result)
 
 func _on_actualizar_jugador_button_down() -> void:
-	pass # Replace with function body.
-
+	var condicion = "nombre = '%s'" % $Nombre.text
+	var datos_nuevos = {
+		"puntaje": int($Puntuación.text)
+	}
+	var resultado = baseDatos.update_rows("players", condicion, datos_nuevos)
+	print("¿Se actualizó?: ", resultado)
+	print("Error (si hay): ", baseDatos.error_message)
 
 func _on_borrar_jugador_button_down() -> void:
-	pass # Replace with function body.
-
+	var condicion = "nombre = '%s'" % $Nombre.text
+	var resultado = baseDatos.delete_rows("players", condicion)
+	print("¿Se borró?: ", resultado)
+	print("Error (si hay): ", baseDatos.error_message)
 
 func _on_consulta_personalizada_button_down() -> void:
-	pass # Replace with function body.
+	baseDatos.query("SELECT * FROM players WHERE puntaje > 50")
+	print("Resultado consulta personalizada: ", baseDatos.query_result)
