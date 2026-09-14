725beeed580e9dd514d79b04bb7f7e35375ad033
luisperezamaro89-cloud <luisperezamaro89@gmail.com>
2026-09-14 17:20:56 -0500

    se areglo todo los botones
    solo falta "Base de datos: SQL Server con 10 registros por tabla"

--- base_datos/SQL.gd
+++ base_datos/SQL.gd
@@ -3,7 +3,7 @@ var baseDatos : SQLite
 
 func _ready():
 	baseDatos = SQLite.new()
-	baseDatos.path = "res://data.db"
+	baseDatos.path = "res://base_datos/data.db"
 	baseDatos.open_db()
 
 func _on_crear_tabla_button_down():
@@ -16,20 +16,30 @@ func _on_crear_tabla_button_down():
 	print("¿Se creó la tabla?: ", resultado)
 	print("Error (si hay): ", baseDatos.error_message)
 
+func nombre_existe(nombre: String) -> bool:
+	baseDatos.query("SELECT * FROM players WHERE nombre = '%s'" % nombre)
+	return baseDatos.query_result.size() > 0
+
 func _on_insertar_jugador_button_down():
+	var nombre_ingresado = $Nombre.text
+	if nombre_existe(nombre_ingresado):
+		print("Este nombre ya está en uso, pruebe con otro o actualícelo")
+		return
 	var data = {
-		"nombre": $Nombre.text,
+		"nombre": nombre_ingresado,
 		"puntaje": int($Puntuación.text)
 	}
 	var resultado = baseDatos.insert_row("players", data)
 	print("¿Se insertó la fila?: ", resultado)
 	print("Error (si hay): ", baseDatos.error_message)
 
-func _on_ver_jugadores_button_down() -> void:
+func _on_ver_jugadores_button_down():
 	baseDatos.query("SELECT * FROM players")
-	print("Jugadores encontrados: ", baseDatos.query_result)
-
-func _on_actualizar_jugador_button_down() -> void:
+	print("--- Lista de Jugadores ---")
+	for jugador in baseDatos.query_result:
+		print("ID: ", jugador["id"], " | Nombre: ", jugador["nombre"], " | Puntaje: ", jugador["puntaje"])
+	print("--------------------------")
+func _on_actualizar_jugador_button_down() :
 	var condicion = "nombre = '%s'" % $Nombre.text
 	var datos_nuevos = {
 		"puntaje": int($Puntuación.text)
@@ -38,12 +48,12 @@ func _on_actualizar_jugador_button_down() -> void:
 	print("¿Se actualizó?: ", resultado)
 	print("Error (si hay): ", baseDatos.error_message)
 
-func _on_borrar_jugador_button_down() -> void:
+func _on_borrar_jugador_button_down():
 	var condicion = "nombre = '%s'" % $Nombre.text
 	var resultado = baseDatos.delete_rows("players", condicion)
 	print("¿Se borró?: ", resultado)
 	print("Error (si hay): ", baseDatos.error_message)
 
-func _on_consulta_personalizada_button_down() -> void:
+func _on_consulta_personalizada_button_down():
 	baseDatos.query("SELECT * FROM players WHERE puntaje > 50")
 	print("Resultado consulta personalizada: ", baseDatos.query_result)
