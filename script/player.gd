f26bc040b48758cbbbb9c04a62839cb83aff3333
jaquelinmartinez042006-boop <jaquelinmartinez042006@gmail.com>
2026-09-17 02:19:28 -0500

    hitbox arreglado

--- script/player.gd
+++ script/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var fuerza_salto: float = 400.0
 
 @export var jugador: int = 1
-@export var vida: int = 100
+@export var vida: int = 500
 
 var jugador_controlador
 
@@ -111,19 +111,33 @@ func _input(event):
 			state_machine.cambiar_estado("Golpear")
 
 func activar_hitbox():
+
+	# Apagar todas
 	collision_arriba.disabled = true
 	collision_abajo.disabled = true
 	collision_delante.disabled = true
 
+	print("================================")
+	print("DIRECCION DEL ATAQUE: ", direccion_ataque)
+
 	match direccion_ataque:
+
 		"arriba":
 			collision_arriba.disabled = false
+			print("HITBOX ARRIBA ACTIVADA")
+			print("ARRIBA disabled: ", collision_arriba.disabled)
 
 		"abajo":
 			collision_abajo.disabled = false
+			print("HITBOX ABAJO ACTIVADA")
+			print("ABAJO disabled: ", collision_abajo.disabled)
 
 		"delante":
 			collision_delante.disabled = false
+			print("HITBOX DELANTE ACTIVADA")
+			print("DELANTE disabled: ", collision_delante.disabled)
+
+	print("================================")
 			
 func desactivar_hitboxes():
 	collision_arriba.disabled = true
