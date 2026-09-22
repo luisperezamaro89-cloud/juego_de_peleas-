42c65e335df71e018a819219045972cd4f26204d
luisperezamaro89-cloud <luisperezamaro89@gmail.com>
2026-09-22 10:49:52 -0500

    control admin casi terminado

--- escenas/Menu_Principal/menu_principal.gd
+++ escenas/Menu_Principal/menu_principal.gd
@@ -7,3 +7,15 @@ func _process(_delta):
 
 func _on_btn_iniciar_partida_pressed() -> void:
 	get_tree().change_scene_to_file("res://escenas/Seleccion/seleccion_personajes.tscn")
+
+
+func _on_administradores_pressed() -> void:
+	$PopUpAdmin/InputPassword.text = "" 
+	$PopUpAdmin.popup_centered(Vector2i(300, 100)) 
+	
+func _on_pop_up_admin_confirmed() -> void:
+	# Cambia "1234" por la contraseña que quieras usar
+		if $PopUpAdmin/InputPassword.text == "Admin123":
+			get_tree().change_scene_to_file("res://base_datos/control.tscn")
+		else:
+			print("Contraseña incorrecta")
